23abfa0a5ddcc230d4e42c5777ff49323149d3b1
Diogo Dores <diogo.dores@gmail.com>
2022-12-03 11:03:57 +0000

    Day 3 - Part 1 and 2

---
+++ Day03/solution.py
@@ -0,0 +1,23 @@
+f = open("input.txt", "r")
+rucksacks = [line.strip() for line in f]
+duplicateItems, badges = [], []
+
+def getPriority(letter):
+    if letter.isupper():
+        return ord(letter) - 38
+    else:
+        return ord(letter) - 96
+
+def chunker(seq, size):
+    return (seq[pos:pos + size] for pos in range(0, len(seq), size))
+
+for items in rucksacks:
+    duplicateItems.append(''.join(set(items[:len(items)//2]).intersection(items[len(items)//2:])))
+
+for elves in chunker(rucksacks, 3):
+    duplicates = (''.join(set(elves[0]).intersection(elves[1])))
+    badges.append(''.join(set(duplicates).intersection(elves[2])))
+
+print('Part 1: ', sum(map(getPriority, duplicateItems)))
+print('Part 2: ', sum(map(getPriority, badges)))
+
