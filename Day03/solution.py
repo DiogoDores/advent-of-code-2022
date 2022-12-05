21575177f91c45e6a46e5a98bc717d4d835eb999
Diogo Dores <diogo.dores@gmail.com>
2022-12-05 00:09:51 +0000

    Clean Day 3 code

--- Day03/solution.py
+++ Day03/solution.py
@@ -9,12 +9,10 @@ def chunker(seq, size):
     return (seq[pos:pos + size] for pos in range(0, len(seq), size))
 
 for items in rucksacks:
-    duplicateItems.append(''.join(set(items[:len(items)//2]).intersection(items[len(items)//2:])))
+    duplicateItems.append(''.join(set(items[:len(items)//2]) & set(items[len(items)//2:])))
 
 for elves in chunker(rucksacks, 3):
-    duplicates = (''.join(set(elves[0]).intersection(elves[1])))
-    badges.append(''.join(set(duplicates).intersection(elves[2])))
+    badges.append(''.join(set(elves[0]) & set(elves[1]) & set(elves[2])))
 
 print('Part 1: ', sum(map(getPriority, duplicateItems)))
 print('Part 2: ', sum(map(getPriority, badges)))
-
