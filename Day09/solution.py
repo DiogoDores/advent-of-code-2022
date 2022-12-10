b6c5a387c3fe9d0f72e3c17354b515178834bc75
Diogo Dores (CTW) <diogo.dores@ctw.bmwgroup.com>
2022-12-10 01:05:42 +0000

    Day 9 - Part 1

--- Day09/solution.py
+++ Day09/solution.py
@@ -1,4 +1,4 @@
-f = open("example.txt", "r")
+f = open("input.txt", "r")
 instructions = [line.split(' ') for line in f.read().splitlines()]
 rope = {'H': [0, 0], 'T': [0, 0]}
 visited = set()
@@ -8,38 +8,36 @@ def isSameColOrRow():
     return rope['T'][0] == rope['H'][0] or rope['T'][1] == rope['H'][1]
 
 def isCloseToHead():
-    top = rope['T'][0] + 1
-    return 
+    tail = rope['T']
+    allPositions = [[tail[0], tail[1]], [tail[0], tail[1] + 1], [tail[0] + 1, tail[1] + 1], [tail[0] + 1, tail[1]], [tail[0] + 1, tail[1] - 1], [tail[0], tail[1] - 1], [tail[0] - 1, tail[1] - 1], [tail[0] - 1, tail[1]], [tail[0] - 1, tail[1] + 1]]
+    return rope['H'] in allPositions
+
+def moveTail():
+    if not isCloseToHead():
+        if rope['H'][1] == rope['T'][1]:
+            rope['T'][0] = rope['T'][0] + 1 if rope['H'][0] > rope['T'][0] else rope['T'][0] - 1
+        elif rope['H'][0] == rope['T'][0]:
+            rope['T'][1] = rope['T'][1] + 1 if rope['H'][1] > rope['T'][1] else rope['T'][1] - 1
+        elif rope['H'][0] > rope['T'][0]:
+            rope['T'][0] += 1
+            rope['T'][1] = rope['T'][1] + 1 if rope['H'][1] > rope['T'][1] else rope['T'][1] - 1
+        elif rope['H'][0] < rope['T'][0]:
+            rope['T'][0] -= 1
+            rope['T'][1] = rope['T'][1] + 1 if rope['H'][1] > rope['T'][1] else rope['T'][1] - 1            
 
 for inst in instructions:
-    oldHeadPos = rope['H']
     for step in range(int(inst[1])):
         if inst[0] == 'U':
             rope['H'][1] += 1
-            if isSameColOrRow():
-                rope['T'][1] += 1
-            else:
-                rope['T'] = oldHeadPos
         elif inst[0] == 'D':
             rope['H'][1] -= 1
-            if isSameColOrRow():
-                rope['T'][1] -= 1
-            else:
-                rope['T'] = oldHeadPos
         elif inst[0] == 'L':
             rope['H'][0] -= 1
-            if isSameColOrRow():
-                rope['T'][0] -= 1
-            else:
-                rope['T'] = oldHeadPos
         elif inst[0] == 'R':
             rope['H'][0] += 1
-            if isSameColOrRow():
-                rope['T'][0] += 1
-            else:
-                rope['T'] = oldHeadPos
+        moveTail()
+        visited.add(tuple(rope['T']))
         print(rope['H'], '|', rope['T'])
-    visited.add(tuple(rope['T']))
     
     
-print(visited)
+print(len(visited))
