506c44c5893045e397fad30f153c7df393332d78
Diogo Dores (CTW) <diogo.dores@ctw.bmwgroup.com>
2022-12-10 14:23:05 +0000

    Day 9 - Part 2

--- Day09/solution.py
+++ Day09/solution.py
@@ -1,43 +1,45 @@
 f = open("input.txt", "r")
 instructions = [line.split(' ') for line in f.read().splitlines()]
-rope = {'H': [0, 0], 'T': [0, 0]}
+
+def isCloseToHead(knot1, knot2):
+    allPositions = [[knot2[0], knot2[1]], [knot2[0], knot2[1] + 1], [knot2[0] + 1, knot2[1] + 1], [knot2[0] + 1, knot2[1]], [knot2[0] + 1, knot2[1] - 1], [knot2[0], knot2[1] - 1], [knot2[0] - 1, knot2[1] - 1], [knot2[0] - 1, knot2[1]], [knot2[0] - 1, knot2[1] + 1]]
+    return knot1 in allPositions
+
+def moveTail(rope):
+    count = 0
+    for knot1, knot2 in zip(rope[:-1], rope[1:]):
+        count += 1
+        if not isCloseToHead(knot1, knot2):
+            if knot1[1] == knot2[1]:
+                knot2[0] = knot2[0] + 1 if knot1[0] > knot2[0] else knot2[0] - 1
+            elif knot1[0] == knot2[0]:
+                knot2[1] = knot2[1] + 1 if knot1[1] > knot2[1] else knot2[1] - 1
+            elif knot1[0] > knot2[0]:
+                knot2[0] += 1
+                knot2[1] = knot2[1] + 1 if knot1[1] > knot2[1] else knot2[1] - 1
+            elif knot1[0] < knot2[0]:
+                knot2[0] -= 1
+                knot2[1] = knot2[1] + 1 if knot1[1] > knot2[1] else knot2[1] - 1
+        if count == len(rope) - 1:
+            visited.add(tuple(knot2))
+
+def followInstructions(rope):
+    for inst in instructions:
+        for _ in range(int(inst[1])):
+            if inst[0] == 'U':
+                rope[0][1] += 1
+            elif inst[0] == 'D':
+                rope[0][1] -= 1
+            elif inst[0] == 'L':
+                rope[0][0] -= 1
+            elif inst[0] == 'R':
+                rope[0][0] += 1
+            moveTail(rope)
+    return len(visited)
+
 visited = set()
 visited.add((0, 0))
-
-def isSameColOrRow():
-    return rope['T'][0] == rope['H'][0] or rope['T'][1] == rope['H'][1]
-
-def isCloseToHead():
-    tail = rope['T']
-    allPositions = [[tail[0], tail[1]], [tail[0], tail[1] + 1], [tail[0] + 1, tail[1] + 1], [tail[0] + 1, tail[1]], [tail[0] + 1, tail[1] - 1], [tail[0], tail[1] - 1], [tail[0] - 1, tail[1] - 1], [tail[0] - 1, tail[1]], [tail[0] - 1, tail[1] + 1]]
-    return rope['H'] in allPositions
-
-def moveTail():
-    if not isCloseToHead():
-        if rope['H'][1] == rope['T'][1]:
-            rope['T'][0] = rope['T'][0] + 1 if rope['H'][0] > rope['T'][0] else rope['T'][0] - 1
-        elif rope['H'][0] == rope['T'][0]:
-            rope['T'][1] = rope['T'][1] + 1 if rope['H'][1] > rope['T'][1] else rope['T'][1] - 1
-        elif rope['H'][0] > rope['T'][0]:
-            rope['T'][0] += 1
-            rope['T'][1] = rope['T'][1] + 1 if rope['H'][1] > rope['T'][1] else rope['T'][1] - 1
-        elif rope['H'][0] < rope['T'][0]:
-            rope['T'][0] -= 1
-            rope['T'][1] = rope['T'][1] + 1 if rope['H'][1] > rope['T'][1] else rope['T'][1] - 1            
-
-for inst in instructions:
-    for step in range(int(inst[1])):
-        if inst[0] == 'U':
-            rope['H'][1] += 1
-        elif inst[0] == 'D':
-            rope['H'][1] -= 1
-        elif inst[0] == 'L':
-            rope['H'][0] -= 1
-        elif inst[0] == 'R':
-            rope['H'][0] += 1
-        moveTail()
-        visited.add(tuple(rope['T']))
-        print(rope['H'], '|', rope['T'])
-    
-    
-print(len(visited))
+print('Part 1: ', followInstructions([[0, 0] for _ in range(2)]))
+visited.clear()
+visited.add((0, 0))
+print('Part 2: ', followInstructions([[0, 0] for _ in range(10)]))
